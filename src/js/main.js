ddcedfc4b46a76ed99f1ac1d8302257bbc6ffbd3
Alexandr <whitewavex@gmail.com>
2018-04-07 15:58:19 +0300

    Slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
+    
+//    BURGER MENU
+    
     $('.open-nav').click(function() {
         $('.page').toggleClass('nav-open');
         $('.burger').toggleClass('open');
         $('.page__wrap').toggleClass('open');
         $(this).toggleClass('open');
+    });
+    
+//    SLICK SLIDER
+    
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false
     });
 });
